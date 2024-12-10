f217e55f09db619660f23ab2c7ebc2fb6508a64d
Nhat-Anh-94 <165351129+Nhat-Anh-94@users.noreply.github.com>
2024-12-10 11:33:06 +0700

    Add files via upload

--- basic.cc
+++ basic.cc
@@ -1,26 +1,3 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2011-2020 Christopher M. Poole <mail@christopherpoole.net>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-// GEANT4 //
 #include "G4VUserDetectorConstruction.hh"
 #include "G4NistManager.hh"
 #include "G4TessellatedSolid.hh"
@@ -34,129 +11,129 @@
 #include "G4Gamma.hh"
 
 #include "G4RunManager.hh"
-#include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
-#include "G4UIExecutive.hh"
 
 #include "G4SystemOfUnits.hh"
 
-// CADMESH //
-//#define CADMESH_LEXER_VERBOSE
-#include "CADMesh.hh"
+// Qt headers
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
 
+// CADMESH //
+#include "CADMesh.hh"
 
 class DetectorConstruction : public G4VUserDetectorConstruction
 {
-  public:
-    G4VPhysicalVolume* Construct()
+public:
+    G4VPhysicalVolume* Construct() override
     {
         // Materials //
-        G4NistManager * nist_manager = G4NistManager::Instance();
-        G4Material * air = nist_manager->FindOrBuildMaterial("G4_AIR");
-        G4Material * water = nist_manager->FindOrBuildMaterial("G4_WATER");
+        G4NistManager* nist_manager = G4NistManager::Instance();
+        G4Material* air = nist_manager->FindOrBuildMaterial("G4_AIR");
+        G4Material* water = nist_manager->FindOrBuildMaterial("G4_WATER");
 
         // World //
-        auto world_solid = new G4Box("world_solid", 2000*mm, 200*cm, 2*m);
-
-        auto world_logical = new G4LogicalVolume( world_solid
-                                                , air
-                                                , "world_logical"
-                                                , 0, 0, 0
-        );
-
+        auto world_solid = new G4Box("world_solid", 2000 * mm, 200 * cm, 2 * m);
+        auto world_logical = new G4LogicalVolume(world_solid, air, "world_logical", nullptr, nullptr, nullptr);
         world_logical->SetVisAttributes(G4VisAttributes::GetInvisible());
 
-        auto world_physical = new G4PVPlacement( 0
-                                               , G4ThreeVector()
-                                               , world_logical
-                                               , "world_physical"
-                                               , 0, false, 0
-        );
- 
+        auto world_physical = new G4PVPlacement(nullptr, G4ThreeVector(), world_logical, "world_physical", nullptr, false, 0);
+
         ////////////////////
         // CADMesh :: STEP //
         ////////////////////
-        auto step_mesh = CADMesh::TessellatedMesh::FromSTL("./Drawing2.stl");  // 
-        step_mesh->SetScale(200);  // 
-        step_mesh->SetOffset(G4ThreeVector(0, 0, 0));  // 
+        auto step_mesh = CADMesh::TessellatedMesh::FromSTL("./Drawing2.stl");  // Read the STL file
+        step_mesh->SetScale(200);  // Set model scale
+        step_mesh->SetOffset(G4ThreeVector(0, 0, 0));  // Set offset position
         auto step_rotation = new G4RotationMatrix();
         step_rotation->rotateY(45 * deg);
 
-        auto step_logical = new G4LogicalVolume(step_mesh->GetSolid(), water, "step_logical", 0, 0, 0);
+        auto step_logical = new G4LogicalVolume(step_mesh->GetSolid(), water, "step_logical", nullptr, nullptr, nullptr);
         new G4PVPlacement(step_rotation, G4ThreeVector(), step_logical, "step_physical", world_logical, false, 0);
 
         return world_physical;
     };
 };
 
-
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
-  private:
+private:
     G4GeneralParticleSource* particle_gun;
 
-  public:
+public:
     PrimaryGeneratorAction()
     {
         particle_gun = new G4GeneralParticleSource();
         particle_gun->SetParticleDefinition(G4Gamma::Definition());
     };
 
-    ~PrimaryGeneratorAction()
+    ~PrimaryGeneratorAction() override
     {
         delete particle_gun;
     };
 
-    void GeneratePrimaries(G4Event* event)
+    void GeneratePrimaries(G4Event* event) override
     {
         particle_gun->GeneratePrimaryVertex(event);
     };
 };
 
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    MainWindow()
+    {
+        // Layout and button for starting the simulation
+        QVBoxLayout* layout = new QVBoxLayout(this);
+        QPushButton* startButton = new QPushButton("Start Simulation", this);
+        layout->addWidget(startButton);
+
+        connect(startButton, &QPushButton::clicked, this, &MainWindow::startSimulation);
+    }
+
+public slots:
+    void startSimulation()
+    {
+        // Initialize Geant4 RunManager
+        auto run_manager = new G4RunManager();
+
+        // Set up DetectorConstruction
+        auto detector_construction = new DetectorConstruction();
+        run_manager->SetUserInitialization(detector_construction);
+
+        // Set up Physics List
+        auto physics_list = new G4VModularPhysicsList();
+        run_manager->SetUserInitialization(physics_list);
+
+        // Set up Primary Generator Action
+        auto primary_generator_action = new PrimaryGeneratorAction();
+        run_manager->SetUserAction(primary_generator_action);
+
+        run_manager->Initialize();  // Initialize simulation
+
+        // Initialize Visualization Manager
+        auto vis_manager = new G4VisExecutive();
+        vis_manager->Initialize();
+
+        // Run the simulation with a number of events (can be adjusted)
+        run_manager->BeamOn(1000);  // Run 1000 events
+
+        delete vis_manager;
+        delete run_manager;
+    }
+};
 
 int main(int argc, char** argv)
 {
-    auto run_manager = new G4RunManager();
+    QApplication app(argc, argv);  // Initialize Qt application
 
-    auto detector_construction = new DetectorConstruction();
-    run_manager->SetUserInitialization(detector_construction);
+    // Create main window
+    MainWindow window;
+    window.setWindowTitle("Geant4 Simulation with Qt");
+    window.show();
 
-    auto physics_list = new G4VModularPhysicsList();
-    run_manager->SetUserInitialization(physics_list);
-
-    auto primary_generator_action = new PrimaryGeneratorAction();
-    run_manager->SetUserAction(primary_generator_action);
-
-    run_manager->Initialize();
-  
-    auto vis_manager = new G4VisExecutive();
-    vis_manager->Initialize();
-
-    auto session = G4UImanager::GetUIpointer();
-
-    // Source //
-    session->ApplyCommand("/gps/pos/type Volume");
-    session->ApplyCommand("/gps/pos/shape Para");
-    session->ApplyCommand("/gps/pos/confine world_physical");
-    session->ApplyCommand("/gps/ang/type iso");
-    session->ApplyCommand("/gps/pos/halfx 2 m");
-    session->ApplyCommand("/gps/pos/halfy 2 m");
-    session->ApplyCommand("/gps/pos/halfz 2 m");
-
-    // Visualisation //
-    session->ApplyCommand("/vis/open OGL 1000x1000-500-50");
-    session->ApplyCommand("/vis/viewer/set/viewpointThetaPhi 70 30");
-    session->ApplyCommand("/vis/viewer/set/style surface");
-    session->ApplyCommand("/vis/drawVolume");
-    session->ApplyCommand("/vis/scene/add/trajectories");
-    session->ApplyCommand("/vis/scene/endOfEventAction accumulate");
-
-    // Run //
-    auto ui = new G4UIExecutive(argc, argv);
-    run_manager->BeamOn(1e2);
-    ui->SessionStart();
-
-    delete session;
-    delete ui;
+    return app.exec();  // Start Qt application event loop
 }
-
